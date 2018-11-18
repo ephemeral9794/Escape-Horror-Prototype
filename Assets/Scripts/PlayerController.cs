b54f257af895a6a256ab3ddee5c9bb22c5c453f2
ephemeral9794 <kthm9794.hiro@gmail.com>
2018-11-18 21:23:12 +0900

    プレイヤーアニメーション仮制作, 素材追加,フォグ追加,各種設定変更

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		animator.speed = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.DownArrow)){
+            animator.speed = 1;
+            animator.SetInteger("Walk", 0); 
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            animator.speed = 1;
+            animator.SetInteger("Walk", 1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)){
+            animator.speed = 1;
+            animator.SetInteger("Walk", 2);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)){
+            animator.speed = 1;
+            animator.SetInteger("Walk", 3);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)){
+            animator.speed = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow)){
+            animator.speed = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow)){
+            animator.speed = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow)){
+            animator.speed = 0;
+        }
+	}
+}
