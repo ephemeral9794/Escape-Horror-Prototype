c6504bf0d9d053b7b62f6792c5e3df22f978dcd6
ephemeral9794 <kthm9794.hiro@gmail.com>
2018-12-11 21:23:28 +0900

    Asset追加、フォルダ整理、グリッドベース移動修正他

---
+++ Assets/Scripts/GridPlayer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPlayer : MonoBehaviour {
+	
+	const int GRID_SPAN = 1;
+	const float SPEED = 2.0f;
+	Vector3 inputDirect;		//入力の向き 
+	Vector3 direction;			//プレーヤーキャラの方向性 
+	Vector3 prePos;				//１フレーム前の状態保存
+	Vector3 pos;				//現フレーム位置
+	Vector3 currentGrid;
+	bool cross;					//グリッド境界
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		cross = false;
+		//あくまで静止状態から初速を得るためだけの入力要求 
+		//ゲームを開始して一度でも方向キーが入力されたらdirectionがVector3.zeroになることは今後一切ない
+		//その意味で以下は起動直後用のみのコード
+		if (direction == Vector3.zero) {
+			direction = GetDirect ();
+		}
+		
+		prePos = transform.position;
+		pos = transform.position; 
+		pos += direction * SPEED * Time.deltaTime; 
+		
+		prePos /= GRID_SPAN;	//1÷3=0.3333になる　この理屈でGRID_SPAN毎に判定が可能となる
+		pos /= GRID_SPAN;
+		
+		if ((int)pos.x != (int)prePos.x) {
+			cross = true;
+		}
+		if ((int)pos.z != (int)prePos.z) {
+			cross = true;
+		}
+		
+		//近いグリッド
+		Vector3 nearGrid = new Vector3 (Mathf.Round (pos.x), pos.y, Mathf.Round (pos.z));
+		
+		// posが「？.01」～「？.49」までの間フラグが立つ（例0.01～0.49の間など） 
+		// forwardPosはフラグ検出用に補助に使われているだけ
+		Vector3 forwardPos = pos + (direction * 0.5f / GRID_SPAN);
+		if (Mathf.RoundToInt (forwardPos.x) != Mathf.RoundToInt (pos.x) ||
+			Mathf.RoundToInt (forwardPos.z) != Mathf.RoundToInt (pos.z)) {
+			//進む先の壁のチェック
+			if (Physics.Raycast (pos, direction, 1.0f * GRID_SPAN)) {
+				//見かけは静止しているが運動量（direction）を弄らず位置を補正し続けている為「静止している訳ではない」 
+				//運動量はVector3.Zero以外の値を持ち続けている事に注意 
+				pos = nearGrid;
+				cross = true;
+			}
+		}
+		
+		//この入力が本当の意味でのゲーム内プレーヤー入力
+		inputDirect = GetDirect ();
+		//入力をプレーヤーの方向性に反映させる　状況により反映方法が変わる
+		if (cross) {
+			if (inputDirect != Vector3.zero) {
+				//入力した方向に曲がれるかどうかの壁チェック 
+				if (!Physics.Raycast (nearGrid, inputDirect, 1.0f * GRID_SPAN)) {
+					direction = inputDirect;
+				}
+			}
+		} else {
+			if (Vector3.Dot (direction, inputDirect) < -0.9999f) {
+				direction = inputDirect;
+			}
+		}
+			
+		this.transform.position = pos;	//最期に補正の有無にかかわらず現フレーム位置を反映させる 
+	}
+	
+	float inputTimer;
+	const float THRESHOLD = 0.3f;
+	const float CONTINUATION = 2.0f;
+	Vector3 preInput;
+	
+	//４方向レバー　無入力時２秒延滞 
+	Vector3 GetDirect ()
+	{
+		inputTimer += Time.deltaTime;
+		Vector3 input = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical"));
+		float absX = Mathf.Abs (input.x); 
+		float absZ = Mathf.Abs (input.z); 
+		
+		if (absX < THRESHOLD && absZ < THRESHOLD) {
+			if (inputTimer < CONTINUATION) {
+				return preInput;
+			}
+			return Vector3.zero;
+		}
+
+		inputTimer = 0;
+		if (absX < absZ) {
+			input.x = 0.0f;
+		} else {
+			input.z = 0.0f;
+		}
+		
+		input = input.normalized;
+		preInput = input;
+		return input;
+	}
+	
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,85 +8,85 @@ using UniRx.Triggers;
 [RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour {
 	[SerializeField]
-	private float speed = 4;
+	private int speed = 12;
 	[SerializeField]
 	private GridLayout grid;
 	
-	private float timeDuration = 0.25f;
-	private Vector3 grid_size;
+	private Vector3 grid_size;	// 1グリッドあたりの大きさ（ワールド座標基準）
+	private Vector3Int direction;	// プレイヤーの向き
+	private Vector3Int prev;
+	private Vector3Int next;
+	private int frames;
 	private Animator animator;
-	private bool walking;
-	private float timeElapsed;
-	private Vector3 start_pos;
-	private Vector3 end_pos;
-    private Vector2 direct;
 
-	// Use this for initialization
-	void Awake () {
-		animator = GetComponent<Animator>();
-		walking = false;
-		animator.SetBool("Walking", walking);
+	private void Start() {
+		direction = Vector3Int.zero;
 		grid_size = grid.CellToWorld(new Vector3Int(1, 1, 0));
-		//transform.position += (grid_size / 2);
-		timeElapsed = 0;
-        direct = new Vector2(0, 0);
-        this.OnKeyAsObservable(KeyCode.UpArrow).Subscribe(_ => direct.y += 1);
-        this.OnKeyAsObservable(KeyCode.DownArrow).Subscribe(_ => direct.y -= 1);
-        this.OnKeyAsObservable(KeyCode.LeftArrow).Subscribe(_ => direct.x += 1);
-        this.OnKeyAsObservable(KeyCode.RightArrow).Subscribe(_ => direct.x -= 1);
-        this.OnKeyDownAsObservable(KeyCode.Space).Subscribe(_ => Debug.Log(direct));
-    }
-
-	void MoveStart(Vector2 direction)
-	{
-		start_pos = transform.position;
-		end_pos = start_pos + new Vector3(grid_size.x * direction.x, grid_size.y * direction.y, grid_size.z);
-		timeElapsed = 0.0f;
-	}
-
-	void Move()
-	{
-		timeElapsed += Time.deltaTime;
-		float rate = timeElapsed / timeDuration;
-		rate = Mathf.Clamp(rate, 0f, 1f);
-		//Lerp：StartPosを0,EndPosを1としたときに、rate(0~１)の位置を返してくれる
-		transform.position = Vector3.Lerp(start_pos, end_pos, rate);
+		prev = grid.WorldToCell(transform.position);
+		next = prev;
+		transform.position = grid.CellToWorld(prev) + new Vector3(0.5f, 0.5f, 0.0f);
+		Debug.Log(grid.CellToWorld(prev));
+		frames = 0;
+		animator = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		// 入力を取得
+		var pos = transform.position;
+		if (Move(ref pos)) {
+			var dir = GetInputDirection();
+			if(dir.x == 0.0f && dir.y == 0.0f) {
+				animator.SetBool("Walking", false);
+			} else {
+				animator.SetBool("Walking", true);
+				animator.SetFloat("DirectionX", dir.x);
+				animator.SetFloat("DirectionY", dir.y);
+			}
+			MovePoint(dir, ref next);
+			//Debug.Log($"{next} {pos}");
+		}
+		transform.position = pos + new Vector3(0.5f, 0.5f, 0.0f);
+		frames++;
+	}
+
+	bool Move(ref Vector3 pos) {
+		float t = (float)frames / speed;
+		var div = next - prev;
+		var wprev = grid.CellToWorld(prev);
+		pos.x = wprev.x + (div.x * grid_size.x) * t;
+		pos.y = wprev.y + (div.y * grid_size.y) * t;
+		//Debug.Log(t);
+
+		if (frames >= speed) {
+			frames = 0;
+			prev = next;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void MovePoint(Vector2 direct, ref Vector3Int point)
+	{
+		// 左右
+		if (direct.x > 0.0f) {
+			point.x += 1;
+		} else if (direct.x < 0.0f) {
+			point.x -= 1;
+		}
+		// 上下
+		if (direct.y > 0.0f) {
+			point.y += 1;
+		} else if (direct.y < 0.0f) {
+			point.y -= 1;
+		}
+	}
+
+	Vector2 GetInputDirection()
+	{
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
-
-        direct.x = Mathf.Clamp01(direct.x);
-        direct.y = Mathf.Clamp01(direct.y);
-
-		// 入力がない場合、待機へ移行
-		if (x == 0.0f && y == 0.0f) {
-			walking = false;
-		} // 入力がある場合、歩きへ移行
-		else {
-			walking = true;
-			// プレイヤーの方向を入力
-			animator.SetFloat("DirectionX", direct.x);
-			animator.SetFloat("DirectionY", direct.y);
-			MoveStart(new Vector2(x, y));
-		}
-		animator.SetBool("Walking", walking);
-
-		// 歩き状態ならプレイヤーを移動
-		if (walking) 
-			Move();
-		//if(walking){
-			/*var rate = timeElapsed / timeDuration;
-			rate = Mathf.Clamp(rate, 0, 1);
-			transform.position = Vector3.Lerp(start_pos, end_pos, rate);
-			if (rate >= 1.0f) {
-				timeElapsed = 0.0f;
-				end_pos += new Vector3(grid_size.x * x, grid_size.y * y, grid_size.z);
-			}*/
-        //}
-		
+		//Debug.Log($"{x}, {y}");
+		return new Vector2(x, y);
 	}
 }
