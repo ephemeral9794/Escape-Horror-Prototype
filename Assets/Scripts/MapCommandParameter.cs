5860a7e4346a2a1898879ca7c49db548c34b1a7a
はかなや <kthm9794.hiro@gmail.com>
2019-01-29 14:52:40 +0900

    マップ修正他

---
+++ Assets/Scripts/MapCommandParameter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EscapeHorror.Prototype { 
+    public interface ICommand {
+        void Execute(Dictionary<MapEventData.Event, string> param);
+    }
+    public class MapCommandParameter : MonoBehaviour
+    {
+        [SerializeField]
+        ICommand[] commands;
+
+        public ICommand[] GetCommands(Type type)
+        {
+            var hit = (ICommand[])commands.Where(c => c.GetType() == type);
+            return hit;
+        }
+    }
+}
