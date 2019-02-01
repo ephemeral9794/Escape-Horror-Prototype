3e0a545be8aeae9b69ceb7e2ed37da310112c1cc
ephemeral9794 <kthm9794.hiro@gmail.com>
2019-02-01 21:11:04 +0900

    DOTween・Excel Importer Maker導入,立ち絵追加,ノベルモード前準備他

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
+
+public class TextController : MonoBehaviour {
+	[SerializeField]
+	private string[] scenario;
+	[SerializeField]
+	private TextMeshProUGUI text;
+
+	int currentLine = 0; // 現在の行番号
+
+	void Start()
+	{
+		TextUpdate();
+		this.OnKeyDownAsObservable(KeyCode.Space).Subscribe(_ => {
+			if (currentLine < scenario.Length) {
+				TextUpdate();
+			}
+		}).AddTo(this);
+	}
+
+	void Update()
+	{
+		// 現在の行番号がラストまで行ってない状態でクリックすると、テキストを更新する
+		/*if (currentLine < scenario.Length && Input.GetMouseButtonDown(0))
+		{
+			TextUpdate();
+		}*/
+	}
+
+	// テキストを更新する
+	void TextUpdate()
+	{
+		// 現在の行のテキストをuiTextに流し込み、現在の行番号を一つ追加する
+		text.text = scenario[currentLine];
+		currentLine++;
+	}
+}
